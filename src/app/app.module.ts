8bde701cff67c414a8b21cf141057f47142f51a7
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-28 17:13:13 +0100

    components and service generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StatusCardComponent } from './status-card/status-card.component';
+import { StatusCardComponent } from './components/status-card/status-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatusCardComponent
+    StatusCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
